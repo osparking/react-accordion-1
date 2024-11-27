8391bb94500b9be81a9a7d7ce4066625a32381ce
osparking <jbpark03@gmail.com>
2024-11-27 11:49:20 +0900

    initial App component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Accordion from "./components/Accordion";
+
+function App() {
+  return (
+    <div className="App">
+      <main>안녕?
+      </main>
+    </div>
+  );
+}
+
+export default App;
