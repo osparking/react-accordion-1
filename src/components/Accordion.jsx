6bde543e5e54ee6c3f414fc3b3b39fe735634e83
osparking <jbpark03@gmail.com>
2024-11-27 11:52:30 +0900

    initial status of Accordion compo'

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+import "./Accordion.css";
+
+const Accordion = ({ items, keepOthersOpen }) => {
+  useEffect(() => items.forEach);
+  const [count, setCount] = useState();
+  return <div className="accordion-parent"></div>;
+};
+
+export default Accordion;
