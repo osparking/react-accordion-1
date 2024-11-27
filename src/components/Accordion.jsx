5b5c61de9c70426246ea828d5911e5c2d9c637e0
osparking <jbpark03@gmail.com>
2024-11-27 15:09:18 +0900

    create accordion compo whole part

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -2,9 +2,59 @@ import { useEffect, useState } from "react";
 import "./Accordion.css";
 
 const Accordion = ({ items, keepOthersOpen }) => {
-  useEffect(() => items.forEach);
+  const [accordionItems, setAccordionItems] = useState(null);
   const [count, setCount] = useState();
-  return <div className="accordion-parent"></div>;
+
+  useEffect(() => {
+    if (items) {
+      setAccordionItems([
+        ...items.map((item) => ({
+          ...item,
+          toggled: false,
+        })),
+      ]);
+    }
+  }, [items]);
+
+  function handleAccordionToggle(clickedItem) {
+    setAccordionItems([
+      ...accordionItems.map((item) => {
+        let toggled = item.toggled;
+        if (clickedItem.id === item.id) {
+          toggled = !item.toggled;
+        } else if (!keepOthersOpen) {
+          toggled = false;
+        }
+        return { ...item, toggled };
+      }),
+    ]);
+  }
+
+  return (
+    <div className="accordion-parent">
+      {accordionItems?.map((listItem, key) => {
+        return (
+          <div
+            className={`accordion ${listItem.toggled ? "toggled" : ""}`}
+            key={key}
+          >
+            <button
+              className="toggle"
+              onClick={() => handleAccordionToggle(listItem)}
+            >
+              <p>{listItem.label}</p>
+              <div className="direction-indicator">
+                {listItem.toggled ? "-" : "+"}
+              </div>
+            </button>
+            <div className="content-parent">
+              <div className="content">{listItem.renderContent()}</div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Accordion;
